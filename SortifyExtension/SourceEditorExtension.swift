72dfc0ac77b1da47ff2f1a8ec88cc01acf686cad
Francisco Amado <franciscoamado@live.com.pt>
2018-09-24 18:10:01 +0100

    Added Intro View to Sortify App

--- SortifyExtension/SourceEditorExtension.swift
+++ SortifyExtension/SourceEditorExtension.swift
@@ -15,7 +15,7 @@ class SourceEditorExtension: NSObject, XCSourceEditorExtension {
         // If your extension needs to do any work at launch, implement this optional method.
     }
     */
-    
+
     /*
     var commandDefinitions: [[XCSourceEditorCommandDefinitionKey: Any]] {
         // If your extension needs to return a collection of command definitions that differs from those in its Info.plist, implement this optional property getter.
