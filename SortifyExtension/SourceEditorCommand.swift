7939793c46f50371d320b21d1d3b0ad551ab7c89
Francisco Amado <franciscoamado@live.com.pt>
2018-09-24 15:42:20 +0100

    Added Sortify App and Source Editor Extension

---
+++ SortifyExtension/SourceEditorCommand.swift
@@ -0,0 +1,36 @@
+//
+//  SourceEditorCommand.swift
+//  SortifyExtension
+//
+//  Created by Francisco Amado on 24/09/2018.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+
+        let selections = invocation.buffer.selections
+        let lines = invocation.buffer.lines
+
+        let selection = selections.compactMap { $0 as? XCSourceTextRange }.first
+
+        if let startingLine = selection?.start.line, let endingLine = selection?.end.line {
+
+            let linesRange = NSRange(location: startingLine, length: endingLine + 1 - startingLine)
+
+            let sortedLines = lines
+                .subarray(with: linesRange)
+                .compactMap { $0 as? String }
+                .sorted(by: <)
+
+            invocation.buffer.lines.replaceObjects(in: linesRange, withObjectsFrom: sortedLines)
+        }
+
+        // Invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        completionHandler(nil)
+    }
+    
+}
---
+++ SortifyExtension/SourceEditorExtension.swift
@@ -0,0 +1,26 @@
+//
+//  SourceEditorExtension.swift
+//  SortifyExtension
+//
+//  Created by Francisco Amado on 24/09/2018.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorExtension: NSObject, XCSourceEditorExtension {
+    
+    /*
+    func extensionDidFinishLaunching() {
+        // If your extension needs to do any work at launch, implement this optional method.
+    }
+    */
+    
+    /*
+    var commandDefinitions: [[XCSourceEditorCommandDefinitionKey: Any]] {
+        // If your extension needs to return a collection of command definitions that differs from those in its Info.plist, implement this optional property getter.
+        return []
+    }
+    */
+    
+}
