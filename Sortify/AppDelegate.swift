e4a34c3b6b5ad10d94c4824e487b114ce8c286ca
Francisco Amado <franciscoamado@live.com.pt>
2018-09-24 12:10:31 +0100

    Initial Commit

---
+++ Sortify/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Sortify
+//
+//  Created by Francisco Amado on 24/09/2018.
+//  Copyright © 2018 franciscoamado. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Sortify/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sortify
+//
+//  Created by Francisco Amado on 24/09/2018.
+//  Copyright © 2018 franciscoamado. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
